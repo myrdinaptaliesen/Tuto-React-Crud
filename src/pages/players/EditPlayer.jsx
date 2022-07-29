337e97b0f9d2caf6dbf52a6a38dce940773beb42
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-07-29 11:21:28 +0100

    Edit et Index Players

--- src/pages/players/EditPlayer.jsx
+++ src/pages/players/EditPlayer.jsx
@@ -1,11 +1,165 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import Menu from "../../components/Menu";
 
 const EditPlayer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  const { player } = useParams();
+  const navigate = useNavigate();
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [height, setHeight] = useState("");
+  const [position, setPosition] = useState("");
+  const [photoPlayer, setphotoPlayer] = useState(null);
+  const [club_id, setClubId] = useState("");
+  const [validationError, setValidationError] = useState({});
+  useEffect(() => {
+    getPlayer();
+  }, []);
+  // GET - Récupère les valeurs de la fiche avec l'API
+  const getPlayer = async () => {
+    await axios
+      .get(`http://localhost:8000/api/players/${player}`)
+      .then((res) => {
+        setfirstName(res.data.firstName);
+        setClubId(res.data.club_id);
+        setlastName(res.data.lastName);
+        setHeight(res.data.height);
+        setPosition(res.data.position);
+        setphotoPlayer(res.data.photoPlayer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const changeHandler = (event) => {
+    setphotoPlayer(event.target.files[0]);
+  };
+  //Fonction d'ajout d'un player
+  const updatePlayer = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("_method", "PATCH");
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+    formData.append("height", height);
+    formData.append("position", position);
+    formData.append("club_id", club_id);
+    formData.append("photoPlayer", photoPlayer);
 
+    if (photoPlayer !== null) {
+      formData.append("photoPlayer", photoPlayer);
+    }
+
+    await axios
+      .post(`http://localhost:8000/api/players/${player}`, formData)
+      .then(navigate("/players"))
+      .catch(({ response }) => {
+        if (response.status === 422) {
+          setValidationError(response.data.errors);
+        }
+      });
+  };
+  return (
+    <div>
+      <Menu />
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-12 col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Modifier un joueur</h4>
+                <hr />
+                <div className="form-wrapper">
+                  {Object.keys(validationError).length > 0 && (
+                    <div className="row">
+                      <div className="col-12">
+                        <div className="alert alert-danger">
+                          <ul className="mb-0">
+                            {Object.entries(validationError).map(
+                              ([key, value]) => (
+                                <li key={key}>{value}</li>
+                              )
+                            )}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <Form onSubmit={updatePlayer}>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="firstName">
+                          <Form.Label>Nom du joueur</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={lastName}
+                            onChange={(event) => {
+                              setlastName(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                        <Form.Group controlId="lastName">
+                          <Form.Label>Prenom du joueur</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={firstName}
+                            onChange={(event) => {
+                              setfirstName(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                        <Form.Group controlId="height">
+                          <Form.Label>Taille du joueur</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={height}
+                            onChange={(event) => {
+                              setHeight(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                        <Form.Group controlId="position">
+                          <Form.Label>Position du joueur</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={position}
+                            onChange={(event) => {
+                              setPosition(event.target.value);
+                            }}
+                          />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="photoPlayer" className="mb-3">
+                          <Form.Label>Photo</Form.Label>
+                          <Form.Control type="file" onChange={changeHandler} />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Button
+                      variant="primary"
+                      className="mt-2"
+                      size="lg"
+                      block="block"
+                      type="submit"
+                    >
+                      Créer
+                    </Button>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default EditPlayer;
--- src/pages/players/Players.jsx
+++ src/pages/players/Players.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Menu from "../../components/Menu";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Players = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    displayPlayers();
+  }, []); // Sans les crochets ça tourne en boucle
 
+  const displayPlayers = async () => {
+    await axios.get("http://localhost:8000/api/players").then((res) => {
+      setPlayers(res.data.data);
+    });
+  };
+  const deletePlayer = (id) => {
+    axios
+      .delete(`http://localhost:8000/api/players/${id}`)
+      .then(displayPlayers);
+  };
+  return (
+    <div>
+      <Menu />
+      <div className="container mt-5">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Nom du joueur</th>
+              <th>Prénom du joueur</th>
+              <th>Taille</th>
+              <th>Position</th>
+              <th>Équipe du joueur</th>
+              <th>Photo du joueur</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players.map((player) => (
+              <tr key={player.id}>
+                <td>{player.firstName}</td>
+                <td>{player.lastName}</td>
+                <td>{player.height}</td>
+                <td>{player.position}</td>
+                <td>{player.nameClub}</td>
+
+                <td>
+                  <img
+                    src={`http://localhost:8000/storage/uploads/${player.photoPlayer}`}
+                    width="75px"
+                  />
+                </td>
+                <td>
+                  <Link
+                    to={`/players/edit/${player.id}`}
+                    className="btn btn-success me-2"
+                  >
+                    Edit
+                  </Link>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      deletePlayer(player.id);
+                    }}
+                  >
+                    Supprimer
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+};
 export default Players;
